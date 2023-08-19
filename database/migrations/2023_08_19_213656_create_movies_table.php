f970f173fb0bf15eb75aa0d1743919473f3fe3a1
andres suarez <pipe1998sando@gmail.com>
2023-08-19 17:16:10 -0500

    firt commit

---
+++ database/migrations/2023_08_19_213656_create_movies_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('descripcion');
+            $table->enum('quality', ['cam', 'FullHD', '720p', '1080p', '4k']);
+            $table->string('image');
+            $table->date('release_year');
+            $table->integer('reproduction_sumber');
+            $table->string('language');
+            $table->string('video_link');
+            $table->string('download_link');
+
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories');
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movies');
+    }
+};
