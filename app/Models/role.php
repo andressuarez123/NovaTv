3745f46bfd44551feac39a828e2b5c7bffb5b7b3
andres suarez <pipe1998sando@gmail.com>
2023-08-26 17:20:12 -0500

    mode finish

---
+++ app/Models/role.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class role extends Model
+{
+  use HasFactory;
+  protected $fillable = [
+     'name',
+     'description',
+
+
+  ];
+
+  //////////////////////////////////////////////////////
+  /////Relaciones
+  public function users(){
+    return $this->hasmany('App\Models\User');
+
+  }
+
+
+
+}
